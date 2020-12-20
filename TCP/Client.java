81e2a29373cc5477eb952570834a8911a3198673
quangvinhbee <72595494+quangvinhbee@users.noreply.github.com>
2020-12-20 13:54:30 +0700

    update

--- TCP/Client.java
+++ TCP/Client.java
@@ -5,10 +5,41 @@
  */
 package TCP;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author lamqu
  */
 public class Client {
-        
+        public static String Bai1(){
+//          Bài 1: Viết chương trình chuyển đổi một số tự nhiên ở hệ cơ số 10 thành số ở hệ cơ số b bất kì (1< b≤ 24). 
+            String str ="";
+            System.out.println("Nhập số tự nhiên: ");
+            String n = String.valueOf(Input.input_int());
+            System.out.println("Nhập cơ số muốn chuyển: ");
+            String coso = String.valueOf(Input.input_int());
+            str=n+"<br>"+coso;
+            return str;
+        }
+        public static void TransferDataTCP(String str){
+            Socket client;
+            try {
+                client = new Socket("localhost", 1234);
+                DataInputStream dis = new DataInputStream(client.getInputStream());
+                DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+                dos.writeUTF(str);//send Dta
+                
+            } catch (IOException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        public static void main(String[] args) {
+            TransferDataTCP(Bai1());
+        }
 }
--- TCP/Server.java
+++ TCP/Server.java
@@ -1,5 +1,11 @@
 package TCP;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +17,31 @@ package TCP;
  * @author lamqu
  */
 public class Server {
-    
+    public static String Bai1(String str){
+        String output="";
+        String[] arrStr = str.split("<br>");
+        
+        return output;
+    }
+    public static String Proccess(String str){
+        String output="";
+        String[] arrStr = str.split("<br>");
+        if(arrStr[0]=="1"){
+            output=Bai1(str);
+        }
+        
+        return output;
+    }
+    public static void TransferDataTCP() throws IOException{
+        ServerSocket server = new ServerSocket(1234);
+        System.out.println("Server đã sẵn sàng");
+        Socket socket = server.accept();
+        
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        
+        String input = dis.readUTF();
+        
+        dos.writeUTF(Proccess(input));
+    } 
 }
