1fa7b4fde1188eefda018548fd3f620c785dad3b
quangvinhbee <72595494+quangvinhbee@users.noreply.github.com>
2020-12-21 18:15:02 +0700

    update

--- TCP/Client.java
+++ TCP/Client.java
@@ -17,29 +17,65 @@ import java.util.logging.Logger;
  * @author lamqu
  */
 public class Client {
+        public static String Bai5(){
+//          Bài 5: Một số được gọi là số thuận nghịch độc nếu ta đọc từ trái sang phải hay từ phải sang trái số đó ta vẫn nhận được một số giống nhau. 
+//          Hãy liệt kê tất cả các số thuận nghịch độc có sáu chữ số (Ví dụ số: 336633). 
+            String str="";
+            System.out.println("Nhập số tự nhiên: ");
+            String n = String.valueOf(Input.input_int());
+            str="5<br>"+n;
+            return str;
+        }
+        public static String Bai4(){
+//          Bài 4: Viết chương trình liệt kê tất cả các số nguyên tố nhỏ hơn n cho trước. 
+            String str="";
+            System.out.println("Nhập số tự nhiên: ");
+            String n = String.valueOf(Input.input_int());
+            str="4<br>"+n;
+            return str;
+        }
+        public static String Bai3(){
+//          Bài 3: Viết chương trình phân tích một số nguyên thành các thừa số nguyên tố Ví dụ: Số 28 được phân tích thành 2 x 2 x 7. 
+            String str="";
+            System.out.println("Nhập số tự nhiên: ");
+            String n = String.valueOf(Input.input_int());
+            str="3<br>"+n;
+            return str;
+        }
+        public static String Bai2(){
+//          Bài 2: Hãy viết chương trình tính tổng các chữ số của một số nguyên bất kỳ. Ví dụ: Số 8545604 có tổng các chữ số là: 8+5+4+5+6+0+4= 32.  
+            String str="";
+            System.out.println("Nhập số tự nhiên: ");
+            String n = String.valueOf(Input.input_int());
+            str="2<br>"+n;
+            return str;
+        }
+    
         public static String Bai1(){
 //          Bài 1: Viết chương trình chuyển đổi một số tự nhiên ở hệ cơ số 10 thành số ở hệ cơ số b bất kì (1< b≤ 24). 
-            String str ="";
+            String str ="1<br>";
             System.out.println("Nhập số tự nhiên: ");
             String n = String.valueOf(Input.input_int());
             System.out.println("Nhập cơ số muốn chuyển: ");
             String coso = String.valueOf(Input.input_int());
-            str=n+"<br>"+coso;
+            str="1<br>"+n+"<br>"+coso;
             return str;
         }
-        public static void TransferDataTCP(String str){
+        public static String TransferDataTCP(String str){
             Socket client;
+            String output="";
             try {
                 client = new Socket("localhost", 1234);
                 DataInputStream dis = new DataInputStream(client.getInputStream());
                 DataOutputStream dos = new DataOutputStream(client.getOutputStream());
                 dos.writeUTF(str);//send Dta
-                
+                output= dis.readUTF();
             } catch (IOException ex) {
                 Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
             }
+            return output;
         }
         public static void main(String[] args) {
-            TransferDataTCP(Bai1());
+            System.out.println(TransferDataTCP(Bai5())); 
         }
 }
--- TCP/Server.java
+++ TCP/Server.java
@@ -3,6 +3,7 @@ package TCP;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import static java.lang.Math.sqrt;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -17,31 +18,138 @@ import java.net.Socket;
  * @author lamqu
  */
 public class Server {
-    public static String Bai1(String str){
+    public static boolean CheckNT(int n){
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean CheckBai5(int n){
+        int m = n;
+        int temp=0;
+        while(m>0){
+            temp=temp*10+m%10;
+            m/=10;
+        }
+        if(n==temp){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static String Bai5(String str){
         String output="";
         String[] arrStr = str.split("<br>");
+        for(int i=100000;i < 999999;i++){
+            if(CheckBai5(i)){
+                output+=i+" ";
+            }
+        }
         
         return output;
+    }
+    public static String Bai4(String str){
+        String output="";
+        String[] arrStr = str.split("<br>");
+        int n = Integer.parseInt(arrStr[1]);
+        if(n==1){
+            output="1";
+        }
+        for(int i=2; i<n; i++){
+            if(CheckNT(i)){
+                output+=i+" ";
+            }
+        }
+        return output;
+    }
+    public static String Bai3(String str){
+        String output="";
+        String[] arrStr = str.split("<br>");
+        int n = Integer.parseInt(arrStr[1]);
+        if(n==1){
+            output="1";
+        }
+        while(n>1){
+            for(int i = 2 ; i<=n;i++){
+                if(CheckNT(i)){
+                    if(n%i==0){
+                        n=n/i;
+                        if(n==1){
+                            output+=i;
+                        }else{
+                            output+=i+"x";
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        return output;
+    }
+    public static String Bai2(String str){
+        String output="";
+        String[] arrStr = str.split("<br>");
+        int n = Integer.parseInt(arrStr[1]);
+        int tong = 0;
+        while(n>0){
+            tong += n % 10;
+            n=n/10;
+        }
+        return output=String.valueOf(tong);
+    }
+    public static String Bai1(String str){
+        String output="";
+        String[] x = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N"};
+        String[] arrStr = str.split("<br>");
+        int n = Integer.parseInt(arrStr[1]);
+        int base = Integer.parseInt(arrStr[2]);
+        while(n>0){
+            int temp = n%base;
+            n/=base;
+            output+=x[temp];
+        } 
+        char[] try1 = output.toCharArray();
+        String temp="";
+        for (int i = try1.length - 1; i >= 0; i--)
+            temp+=try1[i];
+        
+        return temp;
     }
     public static String Proccess(String str){
         String output="";
         String[] arrStr = str.split("<br>");
-        if(arrStr[0]=="1"){
+        if(arrStr[0].equals("1")){
             output=Bai1(str);
+        }else if(arrStr[0].equals("2")){
+            output=Bai2(str);
+        }else if(arrStr[0].equals("3")){
+            output=Bai3(str);
+        }else if(arrStr[0].equals("4")){
+            output=Bai4(str);
+        }else if(arrStr[0].equals("5")){
+            output=Bai5(str);
         }
+        
+            
         
         return output;
     }
     public static void TransferDataTCP() throws IOException{
         ServerSocket server = new ServerSocket(1234);
         System.out.println("Server đã sẵn sàng");
-        Socket socket = server.accept();
+        while(true){
+            Socket socket = server.accept();
         
-        DataInputStream dis = new DataInputStream(socket.getInputStream());
-        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-        
-        String input = dis.readUTF();
-        
-        dos.writeUTF(Proccess(input));
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+
+            String input = dis.readUTF();
+            dos.writeUTF(Proccess(input));
+        }
     } 
+    public static void main(String[] args) throws IOException {
+        TransferDataTCP();
+    }
 }
